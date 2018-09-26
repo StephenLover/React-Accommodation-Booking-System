052eec39895cd9bb45ce2364aaad00a3c5e71e8b
brucewsy <543131561@qq.com>
2018-09-27 01:47:45 +1000

    update the data, remove the unused model

--- Datamodel/data.sh
+++ Datamodel/data.sh
@@ -4,25 +4,7 @@ cd $srcPath
 
 echo "Start the process...."
 
-userNumber=100
-generate=False ## if need to change
-
-if $generate ; then
-	echo "Generate $userNumber email address..."
-	python3 generate_email.py $userNumber
-	echo "Generate tables1...."
-	python3 generate_table.py
-fi
-
 echo "\nStart insert data..."
-python3 insert_test_data.py
-
-if $generate ; then
-	echo "\nGenerate tables2...."
-	python3 generate_table2.py
-fi
-
-echo "\nStart insert data2..."
-python3 insert_test_data2.py
+python3 insert_test_data_new.py
 
 echo "\nFinish....!!"
