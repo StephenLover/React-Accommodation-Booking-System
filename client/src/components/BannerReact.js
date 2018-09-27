b7ce504bc212b43fff9c93808b7241c1fbefa8f5
Jaffar <jaffar@Jaffars-MacBook-Pro.local>
2018-09-27 23:22:35 +0800

    fix index page

--- client/src/components/BannerReact.js
+++ client/src/components/BannerReact.js
@@ -20,13 +20,13 @@ const BannerReact = () => {
 
                 <div className="each_fade">
                     <div className="image_container">
-                        <img className= "banner_img" src={require(`../img/bg-01.jpg`)} />
+                        <img className= "banner_img" src={require(`../img/bg-02.jpg`)} />
                     </div>
                 </div>
 
                 <div className="each_fade">
                     <div className="image_container">
-                        <img className= "banner_img" src={require(`../img/bg-01.jpg`)} />
+                        <img className= "banner_img" src={require(`../img/bg-03.jpg`)} />
                     </div>
                 </div>
             </Fade>
