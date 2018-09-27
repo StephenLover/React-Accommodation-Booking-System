6232c2aab7d6b735da63d1fefcfe6119d69b3a3c
Jaffar <jaffar@Jaffars-MacBook-Pro.local>
2018-09-27 23:19:39 +0800

    fix the accoumdation detail page

--- client/src/components/AccomendationDetailsBanner.js
+++ client/src/components/AccomendationDetailsBanner.js
@@ -12,21 +12,21 @@ const AccomendationDetailsBanner = () => {
     return (
         <div className>
             <Fade {...fadeProperties}>
-                <div className="each_fade">
-                    <div className="image_container">
-                        <img className= "banner_img" src={require(`../img/bg-01.jpg`)} />
+                <div className="each_fade_detail">
+                    <div className="image_container_detail">
+                        <img className= "banner_img_detial" src={require(`../img/bg-01.jpg`)} />
                     </div>
                 </div>
 
-                <div className="each_fade">
-                    <div className="image_container">
-                        <img className= "banner_img" src={require(`../img/bg-02.jpg`)} />
+                <div className="each_fade_detail">
+                    <div className="image_container_detail">
+                        <img className= "banner_img_detial" src={require(`../img/bg-02.jpg`)} />
                     </div>
                 </div>
 
-                <div className="each_fade">
-                    <div className="image_container">
-                        <img className= "banner_img" src={require(`../img/bg-03.jpg`)} />
+                <div className="each_fade_detail">
+                    <div className="image_container_detail">
+                        <img className= "banner_img_detial" src={require(`../img/bg-03.jpg`)} />
                     </div>
                 </div>
             </Fade>
