d63655f599f6e42489d597708bb27cf7de1cc3fa
StephenLover <wwtlove1993@163.com>
2018-10-12 15:00:52 +1100

    merge

--- client/src/components/AccomendationDetailsReviewSingle.js
+++ client/src/components/AccomendationDetailsReviewSingle.js
@@ -24,10 +24,14 @@ class AccomendationDetailsReviewSingle extends Component{
                 <div className="comment_person_detail">
                     <img src={require(`../img/Astrid_1.ico`)} alt="" className="comment_person_img"/>
                     <span id="comment_person_name">{this.state.name}</span>
-                    <span id="comment_person_time">{this.state.transactionDate}</span>
+                    {/* <span id="comment_person_time">{this.state.transactionDate}</span> */}
                     <span id="comment_person_review_date">{this.state.reviewDate}</span>
 
                 </div>
+                <div>
+                    <NonEditableRatingReact rating={this.state.star}/>
+                </div>
+
                 <div className="comment_words">
                     <span>{this.state.reviewComments}</span>
                 </div>
