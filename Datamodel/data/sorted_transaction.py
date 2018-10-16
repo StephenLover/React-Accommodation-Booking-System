b66ec2f706670eb790d6df3d2c35110e2e48b23b
brucewsy <543131561@qq.com>
2018-10-16 13:28:37 +1100

    add the comments into some python files

--- Datamodel/data/sorted_transaction.py
+++ Datamodel/data/sorted_transaction.py
@@ -1,3 +1,10 @@
+#!/usr/bin python
+# -*- coding: utf-8 -*-
+
+'''
+This module is to sort transactionInfo data in transactionInfo.csv file by the createdTime.
+'''
+
 import pandas as pd
 import os
 
@@ -5,10 +12,10 @@ path = 'csv'
 filename = 'transactionInfo.csv'
 
 transactionInfoData = pd.read_csv(os.sep.join([path, filename]))
-print (transactionInfoData.columns)
-print (transactionInfoData[['accommodationId', 'traveler', 'status', 'createdTime']].head())
+#print (transactionInfoData.columns)
+#print (transactionInfoData[['accommodationId', 'traveler', 'status', 'createdTime']].head())
 
 transactionInfoData = transactionInfoData.sort_values("createdTime")
-print (transactionInfoData[['accommodationId', 'traveler', 'status', 'createdTime']].head())
+#print (transactionInfoData[['accommodationId', 'traveler', 'status', 'createdTime']].head())
 
 transactionInfoData.to_csv(os.sep.join([path, filename]),index=False)
