aa2d3dfdc0e5db3113be7fbbddf0862ea8f7ce29
StephenLover <wwtlove1993@163.com>
2018-10-21 19:34:07 +1100

    add google map keys

--- client/src/components/GoogleMapReact.js
+++ client/src/components/GoogleMapReact.js
@@ -4,7 +4,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAAN9UH7rQEgPAbzRIjSDD5NlME-w4v9cs&&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `250px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
